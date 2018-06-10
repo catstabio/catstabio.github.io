dcaf288958e6f0ed68ae7126207e74acf4c99996
catstabio <38704386+catstabio@users.noreply.github.com>
2018-06-09 23:57:47 -0400

    trouble

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,9 +1,8 @@
-var(let) highTemp = document.getElementById("highTemp").innerHTML;
-var(let) lowTemp = document.getElementById("lowTemp").innerHTML; 
+var lowTemp = parseFloat(document.getElementById("lowTemp").innerHTML);
+var highTemp = parseFloat(document.getElementById("highTemp").innerHTML);
 var speed = document.getElementById("windSpeed").innerHTML;
 
-var avgTemp = (highTemp + lowTemp) / 2 
+var avgTemp = (lowTemp + highTemp) / 2; 
 var power = Math.pow(speed,0.16);
-var multiply = 0.6125 * avgTemp 
-var chillFactor = 35.74 + multiply - (35.75*power) + (0.4275*avgTemp*power); 
-
+var chillFactor = 35.74 + (0.6215 * avgTemp) - (35.75 * power) + (0.4275 * avgTemp * power); 
+document.getElementById("windChill").innerHTML = Math.round(chillFactor);
