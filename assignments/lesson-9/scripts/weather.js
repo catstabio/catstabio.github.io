5c212c37284b660bc3f1e25a7a70b0b4f72ae40a
catstabio <38704386+catstabio@users.noreply.github.com>
2018-06-25 22:58:16 -0400

    api

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -1,10 +1,14 @@
-let request = new XMLHttpRequest();
-let requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
-request.open('Get', requestURL, true);
-request.responseType = 'json';
-request.send();
+let weatherRequest = new XMLHttpRequest();
+let apiURL = "https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=88912f95da0c26e69d60ea8f812d73e1";
+weatherRequest.open("GET", apiURL, true);
+weatherRequest.send();
 
-request.onload = function() {
-    let townData = JSON.parse('{"motto":"Where you will grow!"}');
-    document.getElementById('motto').innerHTML = townData.motto;
+weatherRequest.onload = function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    //console.log(weatherData);
+    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+    document.getElementById('script').innerHTML = weatherData.weather.description;
+    document.getElementById('windSpeed').innerHTML = weatherData.wind.speed;
+    let imagesrc = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
+    document.getElementById('weatherimage').src = imagesrc;
 }
