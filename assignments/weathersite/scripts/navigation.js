197339a566edf314143cf17545a2dd6fe767f27a
catstabio <38704386+catstabio@users.noreply.github.com>
2018-07-01 23:50:36 -0500

    new

--- assignments/weathersite/scripts/navigation.js
+++ assignments/weathersite/scripts/navigation.js
@@ -4,10 +4,11 @@
         pageHREF = urlArray[urlArray.length - 1];
 
         if (pageHREF !== "") {
-            menu = document.querySelectorAll('ul#primaryNav li a');
+            menu = document.querySelectorAll('#primaryNav li a');
             for (i = 0; i < menu.length; i++) {
                 currentURL = (menu[i].getAttribute('href'));
                 menu[i].parentNode.className = '';
+                console.log(pageHREF+":"+ currentURL)
                 if (currentURL === pageHREF) {
                     menu[i].parentNode.className = 'active';
                 }
